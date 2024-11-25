57aab39aa1da50f25716eb731010854c70f700fc
mehar221 <150252117+mehar221@users.noreply.github.com>
2024-11-25 12:25:15 +0530

    Update main.tf

--- exercise_0/main.tf
+++ exercise_0/main.tf
@@ -21,7 +21,7 @@ resource "harness_platform_pipeline" "example" {
     branch_name    = "branchName"
     commit_message = "commitMessage"
     file_path      = "filePath"
-    connector_ref  = "connectorRef"
+    connector_ref  = "accountgithubconnector"
     store_type     = "REMOTE"
     repo_name      = "repoName"
   }
@@ -54,7 +54,7 @@ resource "harness_platform_pipeline" "example" {
                           infrastructureDefinition:
                               type: KubernetesDirect
                               spec:
-                                  connectorRef: terraformgitconnector
+                                  connectorRef: accountgithubconnector
                                   namespace: test
                                   releaseName: release-<+INFRA_KEY>
                           allowSimultaneousDeployments: false
@@ -131,7 +131,7 @@ resource "harness_platform_pipeline" "test" {
   git_import_info {
     branch_name   = "main"
     file_path     = ".harness/gitx.yaml"
-    connector_ref = "account.terraformgitconnector"
+    connector_ref = "account.accountgithubconnector"
     repo_name     = "open-repo"
   }
   pipeline_import_request {
