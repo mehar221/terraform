7153823110963b31b4709b88d2361ab3ee14bb09
mehar221 <150252117+mehar221@users.noreply.github.com>
2024-11-26 13:16:34 +0530

    Update variables.tf

--- exercise_1/variables.tf
+++ exercise_1/variables.tf
@@ -1,32 +1,32 @@
 variable "access_key" {
-  type        = "string"
+  type        = string
   default     = "AKIA5KL7XUCJPWTZ4UL7"
 }
 variable "secret_key" {
-  type        = "string"
+  type        = string
   default     = "aZGYmwEOuKertut3rYuMu310BSh2iNoOYtrAvd9f"
 }
 
 variable "region" {
-  type        = "string"
+  type        = string
   description = "AWS Region required by Terraform AWS Provider"
   default     = "us-east-1"
   }
 
 variable "s3_bucket_region" {
-  type        = "string"
+  type        = string
   description = "Region where S3 bucket will be created used to store remote state file"
   default     = "us-east-1"
   }
 
 variable "s3_bucket_name" {
-  type        = "string"
+  type        = string
   description = "Name of the S3 bucket to be created used to store the remote state file"
   default     = "rc-tf-bucket-training-exercise-11"
   }
 
 variable "tag_env" {
-  type        = "string"
+  type        = string
   description = "Tag for Environment Name"
   default     = "training"
   }
